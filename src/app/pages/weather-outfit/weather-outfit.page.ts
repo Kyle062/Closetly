9eb507043c3db8304f0e22025ef34dd607b595d3
Kyle Bernard Trimidal <kylealba0624@gmail.com>
2026-04-19 16:45:33 +0800

    making a skeleton

--- src/app/pages/weather-outfit/weather-outfit.page.ts
+++ src/app/pages/weather-outfit/weather-outfit.page.ts
@@ -1,20 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonIcon,
+  IonList,
+  IonItem,
+  IonThumbnail,
+  IonLabel,
+  IonButton,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  arrowBackOutline,
+  partlySunnyOutline,
+  rainyOutline,
+  sunnyOutline,
+  snowOutline,
+  optionsOutline,
+  scanOutline,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-weather-outfit',
   templateUrl: './weather-outfit.page.html',
   styleUrls: ['./weather-outfit.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonIcon,
+    IonList,
+    IonItem,
+    IonThumbnail,
+    IonLabel,
+    IonButton,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class WeatherOutfitPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    // Register the icons used in the template
+    addIcons({
+      arrowBackOutline,
+      partlySunnyOutline,
+      rainyOutline,
+      sunnyOutline,
+      snowOutline,
+      optionsOutline,
+      scanOutline,
+    });
   }
 
+  ngOnInit() {}
 }
