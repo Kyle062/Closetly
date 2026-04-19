db4caff9b98eb96cfce402497eb94f0fa86f7ea3
Kyle Bernard Trimidal <kylealba0624@gmail.com>
2026-04-19 20:24:05 +0800

    gg

--- src/app/pages/add-item/add-item.page.ts
+++ src/app/pages/add-item/add-item.page.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  arrowBackOutline,
+  optionsOutline,
+  cameraOutline,
+  cloudUploadOutline,
+  chevronDownOutline,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-add-item',
   templateUrl: './add-item.page.html',
   styleUrls: ['./add-item.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonIcon, IonButton, CommonModule, FormsModule],
 })
 export class AddItemPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    // Register the icons used in the template
+    addIcons({
+      arrowBackOutline,
+      optionsOutline,
+      cameraOutline,
+      cloudUploadOutline,
+      chevronDownOutline,
+    });
   }
 
+  ngOnInit() {}
 }
