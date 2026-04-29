6da8c18fb36f7594c6c2d405ea54c0285860b56b
Kyle Bernard Trimidal <kylealba0624@gmail.com>
2026-04-30 06:23:06 +0800

    fixing error designs

--- src/app/pages/about-us/about-us.page.ts
+++ src/app/pages/about-us/about-us.page.ts
@@ -1,20 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { BottomNavComponent } from '../../components/bottom-nav/bottom-nav.component';
+import { IonContent, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  arrowBackOutline,
+  optionsOutline,
+  sunnyOutline,
+  shuffleOutline,
+  calendarOutline,
+  briefcaseOutline,
+  personOutline,
+  mailOutline,
+  callOutline,
+  locationOutline,
+  logoFacebook,
+  logoInstagram,
+  logoTwitter,
+  logoGithub,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.page.html',
   styleUrls: ['./about-us.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonIcon, BottomNavComponent, CommonModule, FormsModule],
 })
 export class AboutUsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {
+    addIcons({
+      arrowBackOutline,
+      optionsOutline,
+      sunnyOutline,
+      shuffleOutline,
+      calendarOutline,
+      briefcaseOutline,
+      personOutline,
+      mailOutline,
+      callOutline,
+      locationOutline,
+      logoFacebook,
+      logoInstagram,
+      logoTwitter,
+      logoGithub,
+    });
   }
 
+  ngOnInit() {}
+
+  goBack() {
+    this.router.navigate(['/profile']);
+  }
+
+  openEmail() {
+    window.open('mailto:KyleAlab79@gmail.com');
+  }
 }
