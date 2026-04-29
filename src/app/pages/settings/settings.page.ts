6c1b8ce3c30619eff8c34c383fd1d50354f5d6ce
Kyle Bernard Trimidal <kylealba0624@gmail.com>
2026-04-30 05:54:51 +0800

    settings page skeleton

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonToggle } from '@ionic/angular/standalone'; // Add IonToggle
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonToggle, CommonModule, FormsModule] // Add to imports
 })
 export class SettingsPage implements OnInit {
 
